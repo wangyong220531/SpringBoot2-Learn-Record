578c5447310639a0ef96a3f7e18d757f98247c68
wang yong <1912573974@qq.com>
2023-05-22 00:20:01 +0800

    first commit

---
+++ src/main/java/com/boot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String handle() {
+        return "Hello AK103";
+    }
+}
